ae585f0b7ce674223c7b7ef1b8ee2841d292035f
creedowl <creedowl@gmail.com>
2020-07-22 18:27:43 +0800

    chore(util): rename func NewLogger to GetLogger

--- util/logger.go
+++ util/logger.go
@@ -4,7 +4,7 @@ import "go.uber.org/zap"
 
 var logger *zap.SugaredLogger
 
-func NewLogger() *zap.SugaredLogger {
+func GetLogger() *zap.SugaredLogger {
 	if logger == nil {
 		_logger, _ := zap.NewDevelopment()
 		logger = _logger.Sugar()
