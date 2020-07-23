7205b665ce4301c94552fbb6cec9731d368604fb
creedowl <creedowl@gmail.com>
2020-07-23 20:47:35 +0800

    refactor(base): update struct Platform & other small changes

--- platform/base.go
+++ platform/base.go
@@ -4,13 +4,17 @@ import (
 	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"live/util"
 )
 
 type Type uint32
 
 const (
-	BILIBILI Type = 1
+	BILIBILI Type = iota
+	DOUYU
 )
+
+var logger = util.GetLogger()
 
 type Danmaku struct {
 	Text  string `json:"text"`
@@ -26,9 +30,9 @@ type Quality struct {
 type Platform struct {
 	Type           Type      `json:"type"`
 	RoomID         uint      `json:"room_id"`
-	Status         uint64    `json:"status"`
-	CurrentQuality uint64    `json:"current_quality"`
-	Links          []string  `json:"links"`
+	Status         uint      `json:"status"`
+	CurrentQuality uint      `json:"current_quality"`
+	Link           string    `json:"link"`
 	Qualities      []Quality `json:"qualities"`
 }
 
@@ -41,44 +45,32 @@ type Room interface {
 	Connect()
 }
 
+// danmaku cache
 var rooms = map[string]Room{}
 
-func selectPlatform(platform Type, roomID, quality uint, client *websocket.Conn) Room {
+func selectPlatform(platform Type, roomID, quality uint, client *websocket.Conn) (Room, error) {
 	switch platform {
 	case BILIBILI:
-		roomID = GetRoomId(roomID)
-		if client == nil {
-			return &Bilibili{
-				RoomID:  roomID,
-				Quality: quality,
-			}
-		}
-		index := fmt.Sprintf("%d:%d", platform, roomID)
-		if rooms[index] == nil {
-			rooms[index] = &Bilibili{
-				Closed:  false,
-				Clients: make(map[*websocket.Conn]bool),
-				RoomID:  roomID,
-			}
-		}
-		rooms[index].AddClient(client)
-		return rooms[index]
+		return GetBilibiliRoom(roomID, quality, client)
+	case DOUYU:
+		return GetDouyuRoom(roomID, quality, client)
 	default:
-		return nil
+		return nil, errors.New(fmt.Sprintf("platform %d not found", platform))
 	}
 }
 
 func InitRoom(platform Type, roomID, quality uint) (*Platform, error) {
-	room := selectPlatform(platform, roomID, quality, nil)
-	if room == nil {
-		return nil, errors.New(fmt.Sprintf("platform %d not found", platform))
+	room, err := selectPlatform(platform, roomID, quality, nil)
+	if err != nil {
+		return nil, err
 	}
 	return room.GetLiveInfo()
 }
 
 func InitDanmaku(platform Type, roomID uint, conn *websocket.Conn) {
-	room := selectPlatform(platform, roomID, 0, conn)
-	if room == nil {
+	room, err := selectPlatform(platform, roomID, 0, conn)
+	if err != nil {
+		logger.Error(err)
 		return
 	}
 	room.Connect()
