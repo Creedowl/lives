0791201680d805e9d6cedd9b4e9b005f923626d0
creedowl <creedowl@gmail.com>
2020-07-25 12:50:29 +0800

    feat(douyu): add danmaku support

--- platform/douyu.go
+++ platform/douyu.go
@@ -1,6 +1,8 @@
 package platform
 
 import (
+	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
@@ -15,31 +17,40 @@ import (
 )
 
 const (
-	BaseUrl = "https://www.douyu.com/%d"
-	RoomUrl = "https://www.douyu.com/lapi/live/getH5Play/%d"
-	DID     = `'10000000000000000000000000001501'`
+	DouyuBaseUrl      = "https://www.douyu.com/%d"
+	DouyuRoomUrl      = "https://www.douyu.com/lapi/live/getH5Play/%d"
+	DouyuDID          = "'10000000000000000000000000001501'"
+	DouyuDanmakuUrl   = "wss://danmuproxy.douyu.com:8501/"
+	DouyuLoginMsg     = "type@=loginreq/roomid@=%d/"
+	DouyuJoinGroupMsg = "type@=joingroup/rid@=%d/gid@=-9999/"
 )
 
 var (
-	roomIDRe     = regexp.MustCompile(`\$ROOM\.room_id\s*=\s*(\d+)`)
-	roomStatusRe = regexp.MustCompile(`\$ROOM\.show_status\s*=\s*(\d+)`)
-	jsRe         = regexp.MustCompile(`<script type="text/javascript">(\s*var[\s\S]*?)</script>`)
+	DouyuRoomIDRe      = regexp.MustCompile(`\$ROOM\.room_id\s*=\s*(\d+)`)
+	DouyuRoomStatusRe  = regexp.MustCompile(`\$ROOM\.show_status\s*=\s*(\d+)`)
+	DouyuJsRe          = regexp.MustCompile(`<script type="text/javascript">(\s*var[\s\S]*?)</script>`)
+	DouyuDanmakuTypeRe = regexp.MustCompile(`type@=(\w+)/?`)
+	DouyuDanmakuTxtRe  = regexp.MustCompile(`txt@=([\s\S]+?)/`)
 )
 
 type Douyu struct {
+	ctx     context.Context
+	cancel  context.CancelFunc
+	Closed  bool
 	RoomID  uint
 	Quality uint
 	Status  int
+	Dan     *websocket.Conn
 	Clients map[*websocket.Conn]bool
 }
 
 func GetDouyuRoom(roomID, quality uint, client *websocket.Conn) (Room, error) {
 	// get real room id
-	html, err := util.Request("GET", fmt.Sprintf(BaseUrl, roomID), "", nil)
+	html, err := util.Request("GET", fmt.Sprintf(DouyuBaseUrl, roomID), "", nil)
 	if err != nil {
 		return nil, err
 	}
-	r := roomIDRe.FindSubmatch(html)
+	r := DouyuRoomIDRe.FindSubmatch(html)
 	if len(r) == 0 {
 		return nil, errors.New(fmt.Sprintf("room %d not found", roomID))
 	}
@@ -50,7 +61,7 @@ func GetDouyuRoom(roomID, quality uint, client *websocket.Conn) (Room, error) {
 	roomID = uint(_roomID)
 
 	// get room status
-	status, err := strconv.Atoi(string(roomStatusRe.FindSubmatch(html)[1]))
+	status, err := strconv.Atoi(string(DouyuRoomStatusRe.FindSubmatch(html)[1]))
 	if err != nil {
 		return nil, err
 	}
@@ -61,7 +72,16 @@ func GetDouyuRoom(roomID, quality uint, client *websocket.Conn) (Room, error) {
 			Status:  status,
 		}, nil
 	}
-	return nil, nil
+	index := fmt.Sprintf("%d:%d", DOUYU, roomID)
+	if rooms[index] == nil {
+		rooms[index] = &Douyu{
+			Closed:  false,
+			Clients: make(map[*websocket.Conn]bool),
+			RoomID:  roomID,
+		}
+	}
+	rooms[index].AddClient(client)
+	return rooms[index], nil
 }
 
 func (d *Douyu) GetLiveInfo() (*Platform, error) {
@@ -73,11 +93,11 @@ func (d *Douyu) GetLiveInfo() (*Platform, error) {
 			CurrentQuality: d.Quality,
 		}, nil
 	}
-	html, err := util.Request("GET", fmt.Sprintf(BaseUrl, d.RoomID), "", nil)
+	html, err := util.Request("GET", fmt.Sprintf(DouyuBaseUrl, d.RoomID), "", nil)
 	if err != nil {
 		return nil, err
 	}
-	matches := jsRe.FindAllSubmatch(html, -1)
+	matches := DouyuJsRe.FindAllSubmatch(html, -1)
 	code := matches[len(matches)-1][1]
 	js, err := pkger.Open("/util/CryptoJS.js")
 	if err != nil {
@@ -92,14 +112,14 @@ func (d *Douyu) GetLiveInfo() (*Platform, error) {
 	vm := otto.New()
 	// use js vm to get params
 	v, err := vm.Run(append(append(cryptoJS, code...),
-		[]byte(fmt.Sprintf("ub98484234(%d,%s,%d)", d.RoomID, DID, time.Now().Unix()))...))
+		[]byte(fmt.Sprintf("ub98484234(%d,%s,%d)", d.RoomID, DouyuDID, time.Now().Unix()))...))
 	if err != nil {
 		return nil, err
 	}
-	logger.Infof("params %s", v.String())
+	logger.Debugf("params %s", v.String())
 	res, err := util.Request(
 		"POST",
-		fmt.Sprintf(RoomUrl, d.RoomID),
+		fmt.Sprintf(DouyuRoomUrl, d.RoomID),
 		fmt.Sprintf("%s&rate=%d", v.String(), d.Quality),
 		map[string]string{
 			"Content-type": "application/x-www-form-urlencoded",
@@ -107,7 +127,7 @@ func (d *Douyu) GetLiveInfo() (*Platform, error) {
 	if err != nil {
 		return nil, err
 	}
-	logger.Info(string(res))
+	logger.Debugf(string(res))
 	data := gjson.ParseBytes(res)
 	var qualities []Quality
 	data.Get("data.multirates").ForEach(func(key, value gjson.Result) bool {
@@ -128,21 +148,194 @@ func (d *Douyu) GetLiveInfo() (*Platform, error) {
 }
 
 func (d *Douyu) AddClient(conn *websocket.Conn) {
-	panic("implement me")
+	d.Clients[conn] = true
+	logger.Infof("add client %+v", conn.RemoteAddr())
+	// listen close event
+	go func() {
+		closed := false
+		conn.SetCloseHandler(func(code int, text string) error {
+			message := websocket.FormatCloseMessage(code, "close")
+			_ = conn.WriteControl(websocket.CloseMessage, message, time.Now().Add(time.Second*5))
+			closed = true
+			d.RemoveClient(conn)
+			logger.Infof("client %+v closed", conn.RemoteAddr())
+			return nil
+		})
+		// do noting here
+		for {
+			if closed || d.Closed {
+				break
+			}
+			_, _, err := conn.ReadMessage()
+			if err != nil {
+				d.RemoveClient(conn)
+			}
+		}
+	}()
 }
 
 func (d *Douyu) RemoveClient(conn *websocket.Conn) {
-	panic("implement me")
+	_ = conn.Close()
+	if _, ok := d.Clients[conn]; ok {
+		delete(d.Clients, conn)
+	}
+	// all clients exited
+	if len(d.Clients) == 0 {
+		d.Close()
+	}
 }
 
-func (d *Douyu) Send(danmaku Danmaku) {
-	panic("implement me")
+func (d *Douyu) Send(danmaku *Danmaku) {
+	logger.Infof("danmaku %+v", danmaku)
+	if len(d.Clients) == 0 {
+		d.Close()
+		return
+	}
+	for client := range d.Clients {
+		err := client.WriteJSON(danmaku)
+		if err != nil {
+			d.RemoveClient(client)
+			continue
+		}
+	}
 }
 
 func (d *Douyu) Close() {
-	panic("implement me")
+	if d.Closed {
+		return
+	}
+	d.Closed = true
+	// stop heartbeat and listener, note listener will not exit until this func is done
+	d.cancel()
+	// close danmaku websocket (douyu)
+	message := websocket.FormatCloseMessage(websocket.CloseNormalClosure, "close")
+	_ = d.Dan.WriteControl(websocket.CloseMessage, message, time.Now().Add(time.Second*5))
+	for client := range d.Clients {
+		_ = client.Close()
+	}
+	delete(rooms, fmt.Sprintf("%d:%d", DOUYU, d.RoomID))
+	logger.Infof("room %d closed, rooms %+v", d.RoomID, rooms)
+}
+
+// danmaku data structure
+// source: https://open.cplusplus.me/DevelopmentDocs/%E6%96%97%E9%B1%BC%E5%BC%B9%E5%B9%95%E6%9C%8D%E5%8A%A1%E5%99%A8%E7%AC%AC%E4%B8%89%E6%96%B9%E6%8E%A5%E5%85%A5%E5%8D%8F%E8%AE%AEv1.6.2.pdf
+// note: 1. date length in first 4 bytes doesn't contain the first 4 bytes
+//       2. one message may contain multi danmaku and one message may be split into multi message
+func (d *Douyu) encode(data []byte) []byte {
+	header := []byte{0, 0, 0, 0, 0, 0, 0, 0, 0xb1, 0x2, 0, 0}
+	binary.LittleEndian.PutUint32(header[0:], uint32(len(data)+9))
+	binary.LittleEndian.PutUint32(header[4:], uint32(len(data)+9))
+	return append(append(header, data...), 0x00)
+}
+
+func (d *Douyu) decode(raw []byte) ([]string, error) {
+	var res []string
+	for offset, length := 0, 0; offset < len(raw); offset += length + 4 {
+		length = int(binary.LittleEndian.Uint32(raw[offset:]))
+		res = append(res, string(raw[offset+12:offset+length+4]))
+	}
+	return res, nil
+}
+
+func (d *Douyu) authenticate() error {
+	// login req
+	err := d.Dan.WriteMessage(websocket.BinaryMessage,
+		d.encode([]byte(fmt.Sprintf(DouyuLoginMsg, d.RoomID))))
+	if err != nil {
+		return err
+	}
+	// join group req, the -9999 group has all danmaku
+	err = d.Dan.WriteMessage(websocket.BinaryMessage,
+		d.encode([]byte(fmt.Sprintf(DouyuJoinGroupMsg, d.RoomID))))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *Douyu) heartBeat() {
+	defer logger.Infof("heartbeat of room %d exited", d.RoomID)
+	data := d.encode([]byte("type@=mrkl/"))
+	ticker := time.NewTicker(time.Second * 45)
+	defer ticker.Stop()
+	for true {
+		select {
+		case <-ticker.C:
+			err := d.Dan.WriteMessage(websocket.BinaryMessage, data)
+			if err != nil {
+				logger.Error(err)
+				d.Close()
+				return
+			}
+		case <-d.ctx.Done():
+			return
+		}
+	}
+}
+
+func (d *Douyu) listener() {
+	defer logger.Infof("listener of room %d exited", d.RoomID)
+	var data []byte
+	for {
+		select {
+		case <-d.ctx.Done():
+			return
+		default:
+			_, raw, err := d.Dan.ReadMessage()
+			if d.Closed {
+				return
+			}
+			if err != nil {
+				logger.Error(err)
+				d.Close()
+				return
+			}
+			data = append(data, raw...)
+			if raw[len(raw)-1] != 0x0 {
+				logger.Debug("half")
+				break
+			}
+			res, err := d.decode(data)
+			if err != nil {
+				logger.Error(err)
+				d.Close()
+				return
+			}
+			data = []byte{}
+			logger.Debugf("room id %d clients %+v", d.RoomID, d.Clients)
+			for _, dan := range res {
+				danmakuType := DouyuDanmakuTypeRe.FindStringSubmatch(dan)[1]
+				if danmakuType == "chatmsg" {
+					d.Send(&Danmaku{
+						Text:  DouyuDanmakuTxtRe.FindStringSubmatch(dan)[1],
+						Color: "#fff",
+						Type:  0,
+					})
+				}
+			}
+		}
+	}
 }
 
 func (d *Douyu) Connect() {
-	panic("implement me")
+	if d.Dan != nil {
+		return
+	}
+	conn, _, err := websocket.DefaultDialer.Dial(DouyuDanmakuUrl, nil)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	logger.Infof("connect to danmaku %d", d.RoomID)
+	d.Dan = conn
+	d.ctx, d.cancel = context.WithCancel(context.Background())
+	// start listener
+	go d.listener()
+	err = d.authenticate()
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	// start heartbeat
+	go d.heartBeat()
 }
